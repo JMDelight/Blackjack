8f6888860a685404f47a64d0d8c4d3aec58bad99
Cory Olson and Joel Delight <cory@>
2016-06-06 14:06:37 -0700

    finished most of the back end logic and the display.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 // business logic
 var suits = ["a", "b", "c", "d"];
 var ranks = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "d", "e"];
-
+var aces = ["&#x1f0a1", "&#x1f0b1", "&#x1f0c1", "&#x1f0d1"]
 var vals = {"a":10, "b":10, "d":10, "e":10, "1":1, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9};
+
 
 var dealerHand = [];
 var playerHand = [];
+var playerHandVal = 0;
 var newDeck = [];
 
 var initDeck = function(arr1, arr2){
@@ -16,6 +18,7 @@ var initDeck = function(arr1, arr2){
       deck.push("&#x1f0" + suit + rank);
     })
   })
+  shuffleDeck(deck);
   return deck;
 }
 
@@ -33,21 +36,25 @@ var calcHand = function(hand) {
   var handValue = 0;
   for (i=0; i < hand.length; i++) {
     var card = hand[i].charAt(7);
+    if (card === "1") {
+      var acePresent = true;
+    };
     handValue += vals[card];
-  }
+  };
+  if (handValue < 12 && acePresent) {
+    handValue += 10;
+  };
   return handValue;
-}
+};
 
 var deal = function(target, deck){
-  // deal 2 cards if init === true
   target.push(deck.pop());
-
-
-  // otherwise deal 1 card to the focus
 }
 
 var stand = function(){
-  // toggle the focus to the dealer
+  while(calcHand(dealerHand) <= 17){
+    deal(dealerHand, newDeck);
+  }
 }
 
 var shuffleDeck = function(arr1) {
@@ -69,12 +76,42 @@ var shuffleDeck = function(arr1) {
   return arr1;
 }
 
+var renderHand = function(hand){
+  var list = "";
 
+  hand.forEach(function(element){
+    list += "<li class='cardface'>" + element + "</li>";
+  });
+
+  return list;
+
+}
 
 
 // display logic
 $(function(){
+  $('.btn-disabled').prop('disabled', true);
 
+  $("#deal").click(function(){
+    deal(playerHand, newDeck)
+    $(".player-hand").empty().append(renderHand(playerHand));
+
+  });
+
+  $("#stand").click(function(){
+    $('#deal').prop('disabled', true);
+    stand()
+    $(".dealer-hand").empty().append(renderHand(dealerHand));
+
+  });
+  $("#new").click(function(){
+    $(".hand").empty();
+    initGame();
+    $('.btn-disabled').prop('disabled', false);
+    $(".dealer-hand").append(renderHand(dealerHand));
+    $(".player-hand").append(renderHand(playerHand));
+
+  });
 
 
 });
